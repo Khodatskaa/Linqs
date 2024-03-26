0b9115f8c1069d26e4197c62e18fc9bbbed3c25a
Khodatskaa <khodatskaalina@gmail.com>
2024-03-26 19:21:07 +0200

    Ex 1 was done

--- Linqs/Program.cs
+++ Linqs/Program.cs
@@ -1,95 +1,110 @@
 ﻿namespace Linqs
 {
-    public static class ArrayQueries
+
+    public static class IntegerQueries
     {
-        // Get an entire array of integers
-        public static int[] GetArray(int[] array)
+        /// <summary>
+        /// Get an entire array of integers.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <returns>Array of integers</returns>
+        public static int[] GetAllIntegers(int[] numbers)
         {
-            return array.ToArray();
+            return numbers;
         }
 
-        // Get the even integers using LINQ query
-        public static int[] GetEvenIntegers(int[] array)
+        /// <summary>
+        /// Get the even integers.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <returns>Array of even integers</returns>
+        public static int[] GetEvenIntegers(int[] numbers)
         {
-            var evenIntegers = from num in array
-                               where num % 2 == 0
-                               select num;
-            return evenIntegers.ToArray();
+            return numbers.Where(n => n % 2 == 0).ToArray();
         }
 
-        // Get the odd integers using LINQ query
-        public static int[] GetOddIntegers(int[] array)
+        /// <summary>
+        /// Get the odd integers.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <returns>Array of odd integers</returns>
+        public static int[] GetOddIntegers(int[] numbers)
         {
-            var oddIntegers = from num in array
-                              where num % 2 != 0
-                              select num;
-            return oddIntegers.ToArray();
+            return numbers.Where(n => n % 2 != 0).ToArray();
         }
 
-        // Get the values greater than the specified using LINQ query
-        public static int[] GetValuesGreaterThan(int[] array, int threshold)
+        /// <summary>
+        /// Get the values greater than the specified.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <param name="specified">Specified value</param>
+        /// <returns>Array of integers greater than the specified value</returns>
+        public static int[] GetValuesGreaterThan(int[] numbers, int specified)
         {
-            var valuesGreaterThanThreshold = from num in array
-                                             where num > threshold
-                                             select num;
-            return valuesGreaterThanThreshold.ToArray();
+            return numbers.Where(n => n > specified).ToArray();
         }
 
-        // Get numbers in a specified range using LINQ query
-        public static int[] GetNumbersInRange(int[] array, int min, int max)
+        /// <summary>
+        /// Get numbers in a specified range.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <param name="min">Minimum value of the range</param>
+        /// <param name="max">Maximum value of the range</param>
+        /// <returns>Array of integers within the specified range</returns>
+        public static int[] GetNumbersInRange(int[] numbers, int min, int max)
         {
-            var numbersInRange = from num in array
-                                 where num >= min && num <= max
-                                 select num;
-            return numbersInRange.ToArray();
+            return numbers.Where(n => n >= min && n <= max).ToArray();
         }
 
-        // Get numbers that are multiples of seven. Sort the result in ascending order using LINQ query
-        public static int[] GetMultiplesOfSevenAscending(int[] array)
+        /// <summary>
+        /// Get numbers that are multiples of seven. Sort the result in ascending order.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <returns>Array of integers that are multiples of seven</returns>
+        public static int[] GetMultiplesOfSevenAscending(int[] numbers)
         {
-            var multiplesOfSevenAscending = from num in array
-                                            where num % 7 == 0
-                                            orderby num ascending
-                                            select num;
-            return multiplesOfSevenAscending.ToArray();
+            return numbers.Where(n => n % 7 == 0).OrderBy(n => n).ToArray();
         }
 
-        // Get numbers that are multiples of eight. Sort the result in descending order using LINQ query
-        public static int[] GetMultiplesOfEightDescending(int[] array)
+        /// <summary>
+        /// Get numbers that are multiples of eight. Sort the result in descending order.
+        /// </summary>
+        /// <param name="numbers">Array of integers</param>
+        /// <returns>Array of integers that are multiples of eight</returns>
+        public static int[] GetMultiplesOfEightDescending(int[] numbers)
         {
-            var multiplesOfEightDescending = from num in array
-                                             where num % 8 == 0
-                                             orderby num descending
-                                             select num;
-            return multiplesOfEightDescending.ToArray();
+            return numbers.Where(n => n % 8 == 0).OrderByDescending(n => n).ToArray();
         }
     }
-    internal class Program
+
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 16, 18, 21, 24, 28 };
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 16, 21, 24, 28, 35, 40 };
 
-            Console.WriteLine("Entire Array:");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetArray(integers)));
+            // Example usage of queries
+            Console.WriteLine("All integers:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetAllIntegers(numbers)));
 
-            Console.WriteLine("\nEven Integers:");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetEvenIntegers(integers)));
+            Console.WriteLine("\nEven integers:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetEvenIntegers(numbers)));
 
-            Console.WriteLine("\nOdd Integers:");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetOddIntegers(integers)));
+            Console.WriteLine("\nOdd integers:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetOddIntegers(numbers)));
 
-            Console.WriteLine("\nValues Greater Than 10:");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetValuesGreaterThan(integers, 10)));
+            Console.WriteLine("\nValues greater than 10:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetValuesGreaterThan(numbers, 10)));
 
-            Console.WriteLine("\nNumbers in Range 5-15:");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetNumbersInRange(integers, 5, 15)));
+            Console.WriteLine("\nNumbers in the range 10 to 20:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetNumbersInRange(numbers, 10, 20)));
 
-            Console.WriteLine("\nMultiples of Seven (Ascending Order):");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetMultiplesOfSevenAscending(integers)));
+            Console.WriteLine("\nMultiples of seven in ascending order:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetMultiplesOfSevenAscending(numbers)));
 
-            Console.WriteLine("\nMultiples of Eight (Descending Order):");
-            Console.WriteLine(string.Join(", ", ArrayQueries.GetMultiplesOfEightDescending(integers)));
+            Console.WriteLine("\nMultiples of eight in descending order:");
+            Console.WriteLine(string.Join(", ", IntegerQueries.GetMultiplesOfEightDescending(numbers)));
         }
     }
+
 }
